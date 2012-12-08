c75e8ec6aaf06cdb05c366afdf843fa607eec6d0
alvinkheng <alvin.k.heng@gmail.com>
2012-12-08 03:26:58 -0800

    closer to montanas mockup

--- chart.js
+++ chart.js
@@ -1,16 +1,19 @@
-var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = 480 - margin.left - margin.right,
-    height = 320 - margin.top - margin.bottom;
+var margin = {top: 20, right: 10, bottom: 20, left: 10},
+    width = 320 - margin.left - margin.right,
+    height = 480 - margin.top - margin.bottom;
 
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 var parseDate = d3.time.format("%Y-%m-%d %I:%M:%S %p").parse;
 
-var x = d3.time.scale()
-    .range([0, width]);
+var x = d3.scale.linear()
+    .domain([-5, 5])
+    .range([0, width])
+    .nice();
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+//Y axis: Time
+var y = d3.time.scale()
+    .range([0, height]);
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -18,11 +21,7 @@ var xAxis = d3.svg.axis()
 
 var yAxis = d3.svg.axis()
     .scale(y)
-    .orient("left");
-
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.bpm); });
+    .orient("right");
 
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -30,13 +29,26 @@ var svg = d3.select("#chart").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var lowBPM = 0;
+var highBPM = 20;
+function getBPMColor(bpm) {
+    var normalizedBPM = Math.max(0, Math.min(bpm/(highBPM-lowBPM), 1));
+    var color = {};
+    color.r = Math.round(normalizedBPM * 255);
+    color.g = Math.round((1-normalizedBPM) * 255);
+    color.b = Math.round(normalizedBPM * 255);
+    return color;
+}
+
 d3.csv("sampleData.csv", function(data, error) {
     //Split each day into its own bucket
     var dailyData = [[]];
     var numDays = 0;
     var currDay = parseDate(data[0].date).getDate();
     data.forEach(function(d) { 
+        //format date
         d.date = parseDate(d.date);
+        //represent bpm as int
         d.bpm = +d.bpm;
         if (currDay != d.date.getDate()) {
             numDays++;
@@ -46,32 +58,34 @@ d3.csv("sampleData.csv", function(data, error) {
         dailyData[numDays].push(d);
     });
     
-    //set x and y scales
-    x.domain(d3.extent(dailyData[0], function(d) { return d.date; }));
-    y.domain([0, d3.max(dailyData[0], function(d) { return d.bpm; })]);
-  
+    //set y scales 
+    y.domain(d3.extent(dailyData[0], function(d) { return d.date; }));
+       
+    //append one day's data to graph
+    svg.selectAll(".bar")
+        .data(dailyData[0])
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", x(-2.5))
+        .attr("width", x(0))
+        .attr("y", function(d) { return y(d.date); })
+        .attr("height", 10)
+        .attr("style", function(d) {
+            var color = getBPMColor(d.bpm);
+            return "fill:rgb("+color.r+","+color.g+","+color.b+")";
+        });
+       
     svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-      .append("text")
-        .text(dailyData[0][0].date);
-
+        .attr("class", "graphHeader")
+    .append("text")
+        .text(dailyData[0][0].date.toDateString());
+    
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("BPM");
-       
-    svg.append("path")
-       .datum(dailyData[0])
-       .attr("class", "line")
-       .attr("d", line)
-       
+        .attr("transform", "translate("+width/2+")");
+    
+    //tool tips
     var dataCirclesGroup = svg.append('svg:g');
     var circles = dataCirclesGroup.selectAll(".data-point")
        .data(dailyData[0]);
